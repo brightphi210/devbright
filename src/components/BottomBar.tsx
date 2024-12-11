fd4d35ba59ca2361f4ee5e24f0459cb72294e346
Bright Philip <chibuzorphilip2001@gmail.com>
2024-12-11 14:45:29 +0100

    Latest

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -5,10 +5,10 @@ import { FaTools } from "react-icons/fa";
 import { TbBriefcaseFilled } from "react-icons/tb";
 
 
-const BottomBar = () => {
+const BottomBar = ({isDark}:any) => {
   return (
     <div className="flex justify-center m-auto" >
-        <div className={`bg-neutral-800 text-white flex items-center fixed w-[90%] lg:hidden bottom-5 py-4 px-6 rounded-full backdrop-filter backdrop-blur-3xl bg-opacity-60 `}>
+        <div className={`${isDark}bg-neutral-800 text-white flex items-center fixed w-[90%] lg:hidden bottom-5 py-4 px-6 rounded-full backdrop-filter backdrop-blur-3xl bg-opacity-60 `}>
             <ul className="flex flex-row justify-between items-center w-full">
                 <li className="bg-neutral-300 flex items-center p-3.5 rounded-full text-lg text-neutral-600"><GoHomeFill /></li>
                 <li className="bg-neutral-300 flex items-center p-3.5 rounded-full text-lg text-neutral-600"><IoMdPerson /></li>
